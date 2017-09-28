9a2981393703e19b0daada0d97ffbadfa13a4a81
baoxd <1459815090@qq.com>
2017-09-28 16:45:56 +0800

    add des

--- Encrypt/src/Test.cpp
+++ Encrypt/src/Test.cpp
@@ -10,9 +10,37 @@
 using namespace std;
 #include <string>
 #include "MD5.h"
+#include "DES.h"
 
 int main() {
-	string s=getMD5("abc");
-	cout << s << endl;
+	// MD5测试
+//	string s=getMD5("abc");
+//	cout << s << endl;
+
+	// DES测试
+	string s = "abcdefgh";
+	string k = "12345678";
+	char out[8];
+	bitset<64> plain = charToBitset(s.c_str());
+	key = charToBitset(k.c_str());
+	// 生成16个子密钥
+	generateKeys();
+	bitset<64> cipher = encrypt(plain);
+	BitsetToChar(cipher, out, 64);
+	cout << "加密前字符串：" << s << endl;
+	cout << "加密后字符串：" << endl;
+	for(int i = 0 ; i < 8; i++)
+	{
+		cout << out[i] ;
+	}
+	// 解密
+	bitset<64> plan;
+	plan = decrypt(cipher);
+	BitsetToChar(plan, out, 64);
+	cout << endl << "解密后数据：" << endl;
+	for(int i = 0 ; i < 8; i++)
+	{
+		cout << out[i] ;
+	}
 	return 0;
 }
